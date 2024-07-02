39bda559c4ef22caf4c66aa5f3f0ce2fc8f11339
jamesrosing <jamesrosing@gmail.com>
2024-07-02 11:21:13 -0700

    updated with Main

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Nav from "../components/ui/nav";
+import Main from "../components/ui/main";
 
 export default function Home() {
   return (
-    <Nav />
+    <>
+    {/* <Nav /> */}
+    <Main />
+    </>
   );
 }
