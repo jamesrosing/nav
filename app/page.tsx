b69c253768edd789df10332b51dabc6916f270f6
jamesrosing <jamesrosing@gmail.com>
2024-07-26 02:05:21 -0700

    updated nav to allure md

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Nav from "../components/ui/nav";
 import Main from "../components/ui/main";
+import Gallery from "../components/ui/gallery";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Main />
+      <Gallery />
     </>
   );
 }
